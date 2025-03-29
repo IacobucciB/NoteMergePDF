6615d4534e9b850344f35a7ffa948d0e8e4bd359
IacobucciB <link00222@gmail.com>
2025-03-29 13:39:32 -0300

    init: created tkinter window with Forest theme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NoteMergePDF
 NoteMergePDF
+
+
---
+++ main.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import ttk
+
+def create_window():
+    window = tk.Tk()
+    window.title("Note Merge PDF")
+    window.option_add("*tearOff", False)
+    window.geometry("800x600")
+
+    window.tk.call("source", "Forest-ttk-theme/forest-dark.tcl")
+    ttk.Style().theme_use("forest-dark")
+    
+    
+    label = ttk.Label(window, text="Hello, Tkinter with Theme!", font=("Roboto", 12))
+    label.pack(pady=20)
+    
+    button = ttk.Button(window, text="Close", command=window.destroy)
+    button.pack(pady=10)
+    
+    window.mainloop()
+
+if __name__ == "__main__":
+    create_window()
