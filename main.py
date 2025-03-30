a262f7122318ed8f3c751ff1278e87beada6486b
IacobucciB <link00222@gmail.com>
2025-03-30 10:54:47 -0300

    refactor: encapsulate window creation in App class for better structure

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import tkinter as tk
 from tkinter import ttk
 
-def create_window():
-    window = tk.Tk()
-    window.tk.call('tk', 'scaling', 2.0)
-    window.title("Note Merge PDF")
-    window.option_add("*tearOff", False)
-    window.geometry("800x600")
+class App:
+    def __init__(self):
+        self.root = self.__create_root()
+    
+    def __create_root(self):
+        root = tk.Tk()
+        root.tk.call('tk', 'scaling', 2.0)
+        root.title("Note Merge PDF")
+        root.option_add("*tearOff", False)
+        root.geometry("800x600")
+        root.tk.call("source", "Forest-ttk-theme/forest-dark.tcl")
+        ttk.Style().theme_use("forest-dark")
+        return root
+    
+    def start_mainloop(self):
+        self.root.mainloop()
 
-    window.tk.call("source", "Forest-ttk-theme/forest-dark.tcl")
-    ttk.Style().theme_use("forest-dark")
-    
-    
-    label = ttk.Label(window, text="Hello, Tkinter with Theme!", font=("Roboto", 12))
-    label.pack(pady=20)
-    
-    button = ttk.Button(window, text="Close", command=window.destroy)
-    button.pack(pady=10)
-    
-    window.mainloop()
 
 if __name__ == "__main__":
-    create_window()
+    app = App()
+    app.start_mainloop()
